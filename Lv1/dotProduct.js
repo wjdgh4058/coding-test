b02e7f9181e2bc89cca9e2596bc6c57f32e4eed5
wjdgh4058 <ljhmoa@gmail.com>
2023-02-21 23:31:42 +0900

    [Lv1] 내적

---
+++ Lv1/dotProduct.js
@@ -0,0 +1,14 @@
+/* 
+길이가 같은 두 1차원 정수 배열 a, b가 매개변수로 주어집니다. a와 b의 내적을 return 하도록 solution 함수를 완성해주세요.
+
+이때, a와 b의 내적은 a[0]*b[0] + a[1]*b[1] + ... + a[n-1]*b[n-1] 입니다. (n은 a, b의 길이)
+*/
+
+function solution(a, b) {
+  var arr = [];
+  for (var i = 0; i < a.length; i++) {
+    arr.push(a[i] * b[i]);
+  }
+  //console.log(arr)
+  return arr.reduce((a, b) => a + b);
+}
